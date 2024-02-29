38c9e6296ae3a8837b0870b13015c7ad321af47e
Scalco7 <fscalco7@gmail.com>
2024-02-29 01:01:13 -0300

    corrections

--- src/app/modules/home/components/card/card.component.ts
+++ src/app/modules/home/components/card/card.component.ts
@@ -1,4 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface ICardData {
+  name: string;
+  description: string;
+  instancyLevel: EInstancyLevel;
+  startDate: Date;
+  doingState: EDoingState;
+}
+
+export enum EDoingState {
+  New = 0,
+  Making = 1,
+  Test = 2,
+  Finish = 3
+}
+
+export enum EInstancyLevel {
+  Tranquilo = 0,
+  Moderado = 1,
+  Urgente = 2
+}
 
 @Component({
   selector: 'app-card',
@@ -7,6 +28,22 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() public card: ICardData = {} as ICardData;
 
+  public instancyText: string = "";
+
+  ngOnInit(): void {
+    switch (this.card.instancyLevel) {
+      case EInstancyLevel.Tranquilo:
+        this.instancyText = "Tranquilo";
+        break;
+      case EInstancyLevel.Moderado:
+        this.instancyText = "Moderado";
+        break;
+      case EInstancyLevel.Urgente:
+        this.instancyText = "Urgente";
+        break;
+    }
+  }
 }
--- src/app/modules/home/page/home/home.component.ts
+++ src/app/modules/home/page/home/home.component.ts
@@ -1,15 +1,68 @@
-import { Component } from '@angular/core';
-import { CardComponent } from '../../components/card/card.component';
+import { Component, OnInit } from '@angular/core';
+import { CardComponent, EDoingState, EInstancyLevel, ICardData } from '../../components/card/card.component';
 import { CardColumnComponent } from '../../components/card-column/card-column.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, CardColumnComponent],
+  imports: [CommonModule, CardComponent, CardColumnComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public cardsList: ICardData[] = [
+    {
+      name: "Fazer comida",
+      description: "Fazer comida para minha mãe pq sou um bom filho",
+      instancyLevel: EInstancyLevel.Urgente,
+      startDate: new Date(),
+      doingState: EDoingState.Making,
+    },
+    {
+      name: "Investir 10K em bitcoin",
+      description: "investir no bitcoin que está subindo",
+      instancyLevel: EInstancyLevel.Tranquilo,
+      startDate: new Date(),
+      doingState: EDoingState.New,
+    },
+    {
+      name: "Estudar programação",
+      description: "Estudar",
+      instancyLevel: EInstancyLevel.Moderado,
+      startDate: new Date(),
+      doingState: EDoingState.Finish,
+    },
+    {
+      name: "Beber água",
+      description: "Ir no bebedouro, pegar um copo e bebr água",
+      instancyLevel: EInstancyLevel.Urgente,
+      startDate: new Date(),
+      doingState: EDoingState.New,
+    },
+    {
+      name: "Fazer comida",
+      description: "Fazer comida para minha mãe pq sou um bom filho",
+      instancyLevel: EInstancyLevel.Moderado,
+      startDate: new Date(),
+      doingState: EDoingState.New,
+    },
+  ];
 
+  public newCardsList: ICardData[] = [];
+  public makingCardsList: ICardData[] = [];
+  public testCardsList: ICardData[] = [];
+  public finishCardsList: ICardData[] = [];
+
+  ngOnInit(): void {
+    this.separateLists();
+  }
+
+  private separateLists(): void {
+    this.newCardsList = this.cardsList.filter(c => c.doingState == EDoingState.New);
+    this.makingCardsList = this.cardsList.filter(c => c.doingState == EDoingState.Making);
+    this.testCardsList = this.cardsList.filter(c => c.doingState == EDoingState.Test);
+    this.finishCardsList = this.cardsList.filter(c => c.doingState == EDoingState.Finish);
+  }
 }
