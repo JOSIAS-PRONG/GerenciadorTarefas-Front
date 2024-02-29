4865e0ff2da2f42e1e3cc81919af0d4e1d3d12a5
Scalco7 <fscalco7@gmail.com>
2024-02-28 23:45:22 -0300

    card-column

---
+++ src/app/modules/home/components/card-column/card-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-column',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-column.component.html',
+  styleUrl: './card-column.component.scss'
+})
+export class CardColumnComponent {
+  @Input() public title: string = "";
+  @Input() public color: string = "";
+
+
+}
--- src/app/modules/home/page/home/home.component.ts
+++ src/app/modules/home/page/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { CardColumnComponent } from '../../components/card-column/card-column.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CardColumnComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
